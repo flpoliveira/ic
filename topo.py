2b4a373de64d855674afec788fd1a33bec0a7207
filipe <filipedasilvadeoliveira@gmail.com>
2020-04-20 17:24:36 -0300

    arp

--- topo.py
+++ topo.py
@@ -3,6 +3,7 @@ from mininet.net import Mininet
 from mininet.util import dumpNodeConnections
 from mininet.log import setLogLevel
 from mininet.cli import CLI
+from mininet.link import TCLink
 from mininet.node import RemoteController, OVSSwitch
 
 class SingleSwitchTopo(Topo):
@@ -24,8 +25,8 @@ class DoubleSwitchTopo(Topo):
         h3 = self.addHost('h3')
         h4 = self.addHost('h4')
 
-        self.addLink(h1, switch)
-        self.addLink(h2, switch)
+        self.addLink(h1, switch, cls=TCLink, bw = 1000)
+        self.addLink(h2, switch, cls=TCLink, bw = 1000)
 
         self.addLink(switch, switch2)
         
@@ -43,7 +44,12 @@ def simpleTest():
         autoSetMacs=True )
    
     net.start()
-   
+    net['h4'].cmd('iperf -s -Z reno -i 1 > resultReno &')
+    net['h3'].cmd('iperf -s -Z cubic -i 1 > resultCubic &')
+
+    net['h1'].cmd('iperf -c 10.0.0.4 -Z reno -t 300 &')
+    net['h2'].cmd('iperf -c 10.0.0.3 -Z cubic -t 300 &')
+    
     CLI ( net )
     net.stop()
 
