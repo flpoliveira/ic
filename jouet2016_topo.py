8fe6fbd173b5bc6572a0695dda5c2905f37b1ab0
filipe <filipedasilvadeoliveira@gmail.com>
2020-05-20 16:41:53 -0300

    Topologia criada

--- jouet2016_topo.py
+++ jouet2016_topo.py
@@ -9,27 +9,29 @@ from mininet.node import RemoteController, OVSSwitch
 
 class NewSwitchTopo(Topo):
     def build(self):
-        Core = self.addSwitch('Core', dpid="0000000000090000")
+        Core = self.addSwitch('Core', dpid="90000")
         Agg = []
-        Agg.append(self.addSwitch('Agg1', dpid="0000000000090001"))
-        Agg.append(self.addSwitch('Agg2', dpid="0000000000090002"))
+        Agg.append(self.addSwitch('Agg1', dpid="90001"))
+        Agg.append(self.addSwitch('Agg2', dpid="90002"))
         ToR = []
-        for i in range(1, 4):
-          ToR.append(self.addSwitch('ToR'+str(i), dpid=("000000000009020"+str(i))))
+        for i in range(1, 5):
+          ToR.append(self.addSwitch('ToR'+str(i), dpid=("9020"+str(i))))
         host = []
         for i in range(1, 41):
           host.append(self.addHost('h'+str(i)))
-        for i in ToR:
+
+        self.addLink(Core, Agg[0], cls=TCLink, bw=1000, delay='1ms', max_queue_size=60)
+        self.addLink(Core, Agg[1], cls=TCLink, bw=1000, delay='1ms', max_queue_size=60)
+      
+        self.addLink(Agg[0], ToR[0], cls=TCLink, bw=1000, delay='0.2ms', max_queue_size=60)
+        self.addLink(Agg[0], ToR[1], cls=TCLink, bw=1000, delay='0.2ms', max_queue_size=60)
+
+        self.addLink(Agg[1], ToR[2], cls=TCLink, bw=1000, delay='0.2ms', max_queue_size=60)
+        self.addLink(Agg[1], ToR[3], cls=TCLink, bw=1000, delay='0.2ms', max_queue_size=60)
+
+        for i in range(0, 4):
           for j in range(0, 10):
-            self.addLink(i, host[j], cls=TCLink, bw=1000, delay='0.1ms')
-        for i in Agg:
-          for j in range(0, 2):
-            self.addLink(i, ToR[j], cls=TCLink, bw=1000, delay='0.2ms')
-        
-        self.addLink(Core, Agg[0], cls=TCLink, bw=1000, delay='1ms')
-        self.addLink(Core, Agg[1], cls=TCLink, bw=1000, delay='1ms')
-       
-        
+            self.addLink(host[i*10+j], ToR[i], cls=TCLink, bw=1000, delay='0.1ms', max_queue_size=60)
 
        
 
